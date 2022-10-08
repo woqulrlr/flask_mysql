555163f1ed530d3dcbfbe71620f3b61c4111dbcf
woqulrlr <event9479@outlook.com>
2022-10-08 15:04:42 +0800

    ver2.0

--- utils.py
+++ utils.py
@@ -1,13 +1,21 @@
 import pymysql
 
-def connection_database():
+database_config = {
+        'host':'localhost',
+        'user':'root',
+        'password':'Qwer@1234',
+        'db':'zabbix',
+        'charset':'utf8',
+        'table':'host_info'}
+
+def _database_connection():
     try:
         connection = pymysql.connect(
-            host = 'localhost',
-            user = 'root',
-            password = 'Qwer@1234',
-            db = 'zabbix',
-            charset = 'utf8',
+            host = database_config['host'],
+            user = database_config['user'],
+            password = database_config['password'],
+            db = database_config['db'],
+            charset = database_config['charset'],
             cursorclass= pymysql.cursors.DictCursor
         )
         print("connection zabbix Database success")
@@ -17,9 +25,25 @@ def connection_database():
         print(e)
         return 0
 
-def close_database(cursor, connection):
+def _database_close(cursor, connection):
     cursor.close()#关闭游标
     connection.close()#关闭连接
     return 0
 
-def 
+def database_read(sql):
+    connection, cursor = _database_connection()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    _database_close(connection, cursor)
+    return result
+
+def database_execute_commit(sql):
+    connection, cursor = _database_connection()
+    try:
+        cursor.execute(sql)
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+    _database_close(connection, cursor)
+    return
+
